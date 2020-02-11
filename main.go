acfe081c4a8815901342221c1766512e11c1de79
8luebottle <itbiz.irs@gmail.com>
2020-02-11 17:09:03 +0900

    WIP : Frontend
    HTML, CSS, JavaScript

--- main.go
+++ main.go
@@ -8,21 +8,23 @@ import (
 	"github.com/labstack/echo"
 )
 
+const fileName string = "jobs.csv"
+
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error {
-	defer os.Remove("jobs.csv")
+	defer os.Remove(fileName)
 	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
 	scrapper.Scrape(term)
-	return c.Attachment("jobs.csv", "jobs.csv")
+	return c.Attachment(fileName, fileName)
 }
 
 func main() {
 	e := echo.New()
 	e.GET("/", handleHome)
+	e.Static("/assets", "assets")
 	e.POST("/scrape", handleScrape)
 	e.Logger.Fatal(e.Start(":1400")) // Start a server
-
 }
